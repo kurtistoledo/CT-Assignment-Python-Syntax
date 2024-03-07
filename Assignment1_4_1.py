9d98f965d0ac3ad72a2da38331a7308f1ed99d99
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 20:06:34 -0600

    Assignment1_4_1

---
+++ Assignment1_4_1.py
@@ -0,0 +1,15 @@
+# 4. Python Dynamic Typing Practice
+# Task 1: Code Correction
+dynamic_variable = "This is a string"
+print(type(dynamic_variable))
+
+dynamic_variable = 100
+print(type(dynamic_variable))
+
+dynamic_variable = 25.5
+print(type(dynamic_variable))
+'''
+<class 'str'>
+<class 'int'>
+<class 'float'>
+'''
