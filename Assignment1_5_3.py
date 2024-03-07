57e3fa8de781e0944d0f405f96b48d37bb9e319a
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 20:24:46 -0600

    Assignment1_5_3

---
+++ Assignment1_5_3.py
@@ -0,0 +1,10 @@
+# Given length and width of the rectangle
+length = 10
+width = 6
+# Area of the rectangle
+area = length * width
+# Perimeter of the rectangle
+perimeter = 2 * (length + width)
+# Print area and perimeter
+print("Area of the rectangle:", area)
+print("Perimeter of the rectangle:", perimeter)
