b4a06216558daafc89a691c806dbfaa021038b7b
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 19:30:23 -0600

    Assignment1_2_1

---
+++ Assignment1_2_1.py
@@ -0,0 +1,9 @@
+# 2. Python Comments & Multi-line Practice
+# Task 1
+# Python, oh Python, so clear and so neat
+# With every new challenge, you're hard to beat.
+# From data to AI, you handle it all
+# Your syntax so simple, yet powerful and tall.
+# Expressive and concise, like poetry in code
+# Python, you're the language where dreams are bestowed.
+  
