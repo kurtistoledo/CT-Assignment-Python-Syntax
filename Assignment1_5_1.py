20e0f29e7319c14f24ab03dfd92c4281a15dd9aa
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 20:14:21 -0600

    Assignment1_5_1

---
+++ Assignment1_5_1.py
@@ -0,0 +1,18 @@
+# 5. Arithmetic Operations in Daily Life
+# Task 1: Grocery Store Math
+# Price
+item1_price = 2.50
+item2_price = 3.50
+item3_price = 1.00 
+# How Many
+item1_quantity = 1
+item2_quantity = 1 
+item3_quantity = 1 
+# Items by Cost
+total_item1_cost = item1_price * item1_quantity
+total_item2_cost = item2_price * item2_quantity
+total_item3_cost = item3_price * item3_quantity
+# Total Cost
+total_cost = total_item1_cost + total_item2_cost + total_item3_cost
+
+print("Total cost of the three items: $",total_cost)
