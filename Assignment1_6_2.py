0772aba0458df0341a9609a33053ba4a42737ddb
kurtistoledo <158211620+kurtistoledo@users.noreply.github.com>
2024-03-06 21:11:22 -0600

    Assignment1_6_2

---
+++ Assignment1_6_2.py
@@ -0,0 +1,11 @@
+# 6. Understanding Assignments and Comparisons
+# Task 2: Perfect Square Checker
+number = 14
+square_root = number ** 0.5
+
+if square_root * square_root == number:
+    print(number, "is a perfect square.")
+else:
+    print(number, "is not a perfect square.")
+
+# 14 is a perfect square.
