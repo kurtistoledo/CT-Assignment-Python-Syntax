bcf6248d35f58af62c725ce599298ffa1222de1a
kurtistoledo <158211620+kurtistoledo@users.noreply.github.com>
2024-03-06 21:30:54 -0600

    Assignment1_7_1.py

---
+++ Assignment1_7_1.py
@@ -0,0 +1,15 @@
+# 7. Exploring Logical Operations and Precedence
+# Task 1: Simple Logic Puzzles
+value1 = True
+value2 = False
+
+desired_outcome1 = (value1 and value2)
+desired_outcome2 = (value1 or value2)
+
+print("Desired outcome 1:", desired_outcome1)
+print("Desired outcome 2:", desired_outcome2)
+
+'''
+Desired outcome 1: False
+Desired outcome 2: True
+'''
