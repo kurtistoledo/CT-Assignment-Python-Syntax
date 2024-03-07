1310f10374abb02e82e0aac5c0cfb791dde237be
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 19:50:18 -0600

    Assignment1_3_2

---
+++ Assignment1_3_2.py
@@ -0,0 +1,19 @@
+# 3. Python Naming Convention Practice
+# Task 2: Code Correction
+variable_a = "Hello, World!"
+print(type(variable_a))
+
+variable_b = 23
+print(type(variable_b))
+
+variable_c = 3.14
+print(type(variable_c))
+
+variable_d = True
+print(type(variable_d))
+'''
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+'''
