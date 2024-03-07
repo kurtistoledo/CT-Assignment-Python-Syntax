099dc6804ac4af47b345ad5a0ce97a7f14aa9486
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 19:34:21 -0600

    Assignment1_2_3

---
+++ Assignment1_2_3.py
@@ -0,0 +1,9 @@
+# 2. Python Comments & Multi-line Practice
+# Task 3
+# Single-line comment from the first poem
+# Python, oh Python, so clear and so neat
+
+# Multi-line comment from the second poem
+'''
+From scripting tasks to complex algorithms, you excel,
+'''
