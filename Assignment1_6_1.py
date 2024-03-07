7168b7df5f24fa54b39f1e54e839a5df01cab1db
kurtistoledo <158211620+kurtistoledo@users.noreply.github.com>
2024-03-06 21:02:03 -0600

    Assignment1_6_1.py

---
+++ Assignment1_6_1.py
@@ -0,0 +1,14 @@
+# 6. Understanding Assignments and Comparisons
+# Task 1: Value Swapping
+a = 3
+b = 4
+
+# Swap the values using assignment operators
+a = a + b
+b = a - b
+a = a - b
+
+# Print the swapped values
+print("Swapped values: a =", a, ", b =", b)
+
+#Swapped values: a = 4 , b = 3
