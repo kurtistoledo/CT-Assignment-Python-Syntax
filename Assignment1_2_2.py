f73ff31fed37f7a83e3d512dcf2f7911619fac71
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 19:31:58 -0600

    Assignment1_2_2

---
+++ Assignment1_2_2.py
@@ -0,0 +1,10 @@
+# 2. Python Comments & Multi-line Practice
+# Task 2
+'''
+Python, in the realm of code you shine,
+With simplicity and grace, you're truly divine.
+From scripting tasks to complex algorithms, you excel,
+In the world of programming, you cast a magical spell.
+Python, the language of innovation and wonder,
+In your embrace, programmers thrive and ponder.
+'''
