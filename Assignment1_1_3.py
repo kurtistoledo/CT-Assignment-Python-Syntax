ce5923615c480cf4dc42fee56fc59c21a6876861
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 19:24:58 -0600

    Assignment1_1_2&3

---
+++ Assignment1_1_3.py
@@ -0,0 +1,9 @@
+# 1. Showcase Your Dance Moves!
+# Task 3
+mood = "excited"
+
+if mood == "excited":
+    print("Yay! Let's have fun.")
+else:
+    print("Let's find something fun to do!")
+# No, the indentation was incorrect
