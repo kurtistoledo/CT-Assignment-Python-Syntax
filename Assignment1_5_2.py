a00ee4bcd77a010c58cc9f021d1a9ce7a97fee06
Kurtis Toledo <kurtistoledo@gmail.com>
2024-03-06 20:24:29 -0600

    Assignment1_5_2

---
+++ Assignment1_5_2.py
@@ -0,0 +1,13 @@
+# 5. Arithmetic Operations in Daily Life
+# Task 2: Bank Interest
+
+# Initial amount in the savings account
+initial_amount = 2000  # Example initial amount
+# Fixed yearly interest rate (expressed as a decimal)
+
+interest_rate = 0.05  # 5% yearly interest rate
+# Calculate the total amount in the account after a year
+
+total_amount = initial_amount * (1 + interest_rate)
+# Print the total amount after a year
+print("Total in your savings account after a year:", total_amount)
